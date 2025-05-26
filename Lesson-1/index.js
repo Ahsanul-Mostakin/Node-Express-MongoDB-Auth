868276d9e6c3391e0a9db1a78e24e4f125813269
Ahsanul Mostakin <ahsanulmostakin1111@gmail.com>
2025-05-26 11:59:01 +0600

    practice

---
+++ Lesson-1/index.js
@@ -0,0 +1,4 @@
+const { getName } = require("./students");
+// console.log(s1.getName());
+// console.log(s1.getAge());
+console.log(getName());
---
+++ Lesson-1/students.js
@@ -0,0 +1,19 @@
+exports.getName = () => {
+  return "Ahsanul Mostakin";
+};
+
+const getAge = () => {
+  return 25;
+};
+
+const cgpa = 3.05;
+
+// exports.getName = getName;
+// exports.getAge = getAge;
+// exports.result = cgpa;
+
+// module.exports = {
+//   getName,
+//   getAge,
+//   cgpa,
+// };
