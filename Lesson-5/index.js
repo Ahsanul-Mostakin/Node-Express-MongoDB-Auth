961cabfb54a0c9a0fed026f4cda3fa77957d7745
Ahsanul Mostakin <ahsanulmostakin1111@gmail.com>
2025-05-27 12:00:30 +0600

    practice

---
+++ Lesson-5/index.js
@@ -0,0 +1,32 @@
+const http = require("http");
+const fs = require("fs");
+const PORT = 3000;
+const hostName = "127.0.0.1";
+
+const server = http.createServer((req, res) => {
+  const handleReadFile = (statusCode, fileLocation) => {
+    fs.readFile(fileLocation, (err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("Internal Server Error");
+        return;
+      }
+      res.writeHead(statusCode, { "Content-Type": "text/html" });
+      res.end(data);
+    });
+  };
+
+  if (req.url === "/") {
+    handleReadFile(200, "./views/index.html");
+  } else if (req.url === "/about") {
+    handleReadFile(200, "./views/about.html");
+  } else if (req.url === "/contact") {
+    handleReadFile(200, "./views/contact.html");
+  } else {
+    handleReadFile(404, "./views/error.html");
+  }
+});
+
+server.listen(PORT, hostName, () => {
+  console.log(`Server is running at http://${hostName}:${PORT}`);
+});
