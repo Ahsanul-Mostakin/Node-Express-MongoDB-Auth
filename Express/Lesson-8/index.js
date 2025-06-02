ce436bb7c8c10c0d52ace506e55cf42ff830f01c
Ahsanul Mostakin <ahsanulmostakin1111@gmail.com>
2025-06-02 17:56:48 +0600

    Mini project with express.js

--- Express/Lesson-8/index.js
+++ Express/Lesson-8/index.js
@@ -6,10 +6,14 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.post("/user", (req, res) => {
-  const name = req.body.name;
+app.get("/register", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/register", (req, res) => {
+  const fullName = req.body.fullName;
   const age = req.body.age;
-  res.send(`Welcome ${name}. ${name} is ${age} years old. `);
+  res.send(`<h2>Your name is ${fullName}. Age is ${age}</h2>`);
 });
 
 app.listen(PORT, () => {
