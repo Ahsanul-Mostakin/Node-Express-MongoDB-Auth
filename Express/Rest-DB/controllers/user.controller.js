617132317ac0e051a860a3ca828556b7287b79a3
Ahsanul Mostakin <ahsanulmostakin1111@gmail.com>
2025-07-02 17:29:32 +0600

    practice

---
+++ Express/Rest-DB/controllers/user.controller.js
@@ -0,0 +1,36 @@
+const getAllUsers = (req, res) => {
+  res.status(200).json({
+    message: "all users",
+  });
+};
+
+const getOneUser = (req, res) => {
+  res.status(200).json({
+    message: "get one user",
+  });
+};
+
+const createUser = (req, res) => {
+  res.status(201).json({
+    message: "user is created",
+  });
+};
+
+const updateUser = (req, res) => {
+  res.status(200).json({
+    message: "user is updated",
+  });
+};
+
+const deleteUser = (req, res) => {
+  res.status(200).json({
+    message: "user is deleted",
+  });
+};
+module.exports = {
+  getAllUsers,
+  getOneUser,
+  createUser,
+  updateUser,
+  deleteUser,
+};
