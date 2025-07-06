4672f8bbcb0985898087e42741c5131cec615ac3
Ahsanul Mostakin <ahsanulmostakin1111@gmail.com>
2025-07-07 05:49:27 +0600

    chalk npm color package

--- Express/Morgan-Middleware/index.js
+++ Express/Morgan-Middleware/index.js
@@ -1,17 +1,14 @@
 const express = require("express");
-const morgan = require("morgan");
+const chalk = require("chalk");
 
 const app = express();
-
-app.use(morgan("dev"));
 
 app.get("/products", (req, res) => {
   res.send("List all the products!");
 });
-app.post("/products", (req, res) => {
-  res.status(201).send("create a product!");
-});
 
 app.listen(3000, () => {
-  console.log(`server is running at http://localhost:3000`);
+  console.log(
+    chalk.blue.bgRed.bold(`server is running at http://localhost:3000`)
+  );
 });
