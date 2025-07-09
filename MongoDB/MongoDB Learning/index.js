08358d900722c4253152807c0b2b896d0c8a8056
Ahsanul Mostakin <ahsanulmostakin1111@gmail.com>
2025-07-09 08:19:29 +0600

    schema and model

--- MongoDB/MongoDB Learning/index.js
+++ MongoDB/MongoDB Learning/index.js
@@ -4,6 +4,21 @@ const app = express();
 
 const port = 3002;
 
+// create product schema
+const productSchema = new mongoose.Schema({
+  tittle: String,
+
+  price: Number,
+  describtion: String,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// create Product model
+const Product = mongoose.model("Products", productSchema);
+
 // mongoose
 //   .connect("mongodb://127.0.0.1:27017/testProductDB")
 //   .then(() => console.log("db is connected"))
