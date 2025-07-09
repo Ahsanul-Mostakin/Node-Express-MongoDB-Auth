d673dfe6de12ebe4005b927fe5e0479c6d2ba14f
Ahsanul Mostakin <ahsanulmostakin1111@gmail.com>
2025-07-09 10:37:51 +0600

    find single | multiple data from database

--- MongoDB/MongoDB Learning/index.js
+++ MongoDB/MongoDB Learning/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const { title } = require("process");
 const app = express();
 
 const port = 3002;
@@ -59,26 +60,26 @@ app.post("/products", async (req, res) => {
   try {
     // get data from request body
 
-    // const newProduct = new Product({
-    //   title: req.body.title,
-    //   price: req.body.price,
-    //   description: req.body.description,
-    // });
+    const newProduct = new Product({
+      title: req.body.title,
+      price: req.body.price,
+      description: req.body.description,
+    });
 
-    const productData = await Product.insertMany([
-      {
-        title: "iphone 5",
-        price: 250,
-        description: "beautiful phone",
-      },
-      {
-        title: "iphone 6",
-        price: 25,
-        description: "beautiful phone",
-      },
-    ]);
+    // const productData = await Product.insertMany([
+    //   {
+    //     title: "iphone 5",
+    //     price: 250,
+    //     description: "beautiful phone",
+    //   },
+    //   {
+    //     title: "iphone 6",
+    //     price: 25,
+    //     description: "beautiful phone",
+    //   },
+    // ]);
 
-    newProduct.save();
+    const productData = await newProduct.save();
     res.status(201).send({ productData });
   } catch (error) {
     res.status(500).send({
@@ -86,6 +87,61 @@ app.post("/products", async (req, res) => {
     });
   }
 });
+
+app.get("/products", async (req, res) => {
+  try {
+    const products = await Product.find();
+    if (products) {
+      res.status(200).send({
+        success: true,
+        message: "return all products",
+        data: products,
+      });
+    } else {
+      success: false,
+        res.status(404).send({
+          message: "products not found",
+        });
+    }
+  } catch (error) {
+    res.status(500).send({
+      message: error.message,
+    });
+  }
+});
+
+app.get("/products/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const product = await Product.findOne({ _id: id });
+
+    if (product) {
+      res.status(200).send({
+        success: true,
+        message: "return single product",
+        data: product,
+      });
+    } else {
+      success: false,
+        res.status(404).send({
+          message: "product not found",
+        });
+    }
+  } catch (error) {
+    res.status(500).send({
+      message: error.message,
+    });
+  }
+});
+
+// const loadRegister = async (req, res) => {
+//   try {
+//   } catch (error) {
+//     res.status(500).send({
+//       message: error.message,
+//     });
+//   }
+// };
 
 app.listen(port, async () => {
   console.log(`Server is running at http://localhost:${port}`);
