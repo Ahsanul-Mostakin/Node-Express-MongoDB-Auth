950a1ebf03e0be5c048cd9599563f2d666615894
Ahsanul Mostakin <ahsanulmostakin1111@gmail.com>
2025-07-09 07:47:28 +0600

    Connection with database

---
+++ MongoDB/MongoDB Learning/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+const port = 3002;
+
+// mongoose
+//   .connect("mongodb://127.0.0.1:27017/testProductDB")
+//   .then(() => console.log("db is connected"))
+//   .catch((error) => {
+//     console.log("db is not connected");
+//     console.log(error);
+//     process.exit(1);
+//   });
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/testProductDB");
+    console.log("db is connected");
+  } catch (error) {
+    console.log("db is not connected");
+    console.log(error.message);
+    process.exit(1);
+  }
+};
+
+app.get("/", (req, res) => {
+  res.send("welcome to home page!");
+});
+
+app.listen(port, async () => {
+  console.log(`Server is running at http://localhost:${port}`);
+  await connectDB();
+});
